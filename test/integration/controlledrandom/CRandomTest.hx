91d8a2d4d113e403e9eb6014e04f5cfc798f3eac
Wesss <wesleymcox@gmail.com>
2019-01-12 18:13:17 -0800

    1 value integration test

--- test/integration/controlledrandom/CRandomTest.hx
+++ test/integration/controlledrandom/CRandomTest.hx
@@ -15,17 +15,27 @@ class CRandomTest extends haxe.unit.TestCase {
         return Std.int((randVal / 233281.0) * max);
     }
 
-    public function testNumbersStayInBounds() {
-        // but it fits our needs as a seeded RNG for some basic tests
+    public function testFourValuesNumbersStayInBounds() {
+        // TODO migrate to something with before each support
         randVal = 0.0;
 
         var cRand: CRandom = CRandom.createWithRandom(100, pseudoRandomSeededInt);
 
         for (i in 0...10000) {
             var randValue = cRand.next();
-            trace(randValue);
             assertTrue(randValue < 100);
             assertTrue(randValue > -1);
+        }
+    }
+
+    public function testOneValueNumbersStayInBounds() {
+        // TODO migrate to something with before each support
+        randVal = 0.0;
+
+        var cRand: CRandom = CRandom.createWithRandom(1, pseudoRandomSeededInt);
+
+        for (i in 0...10000) {
+            assertTrue(cRand.next() == 0);
         }
     }
 }
